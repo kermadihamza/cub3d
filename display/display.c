a1235c119a7dc9e1fbccab656a8b827a094a9273
Hamza Kermadi <hakermad@mi-r4-p1.s19.be>
2022-09-16 18:58:07 +0200

    :construction: colisiona wall not good2

--- display/display.c
+++ display/display.c
@@ -6,7 +6,7 @@
 /*   By: hakermad <hakermad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 20:51:29 by lrondia           #+#    #+#             */
-/*   Updated: 2022/09/14 17:04:58 by hakermad         ###   ########.fr       */
+/*   Updated: 2022/09/16 18:08:06 by hakermad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,44 +32,13 @@ void	angle_changes(int code, t_ray *ray)
 	}
 }
 
-// int	block_wall(int code, t_game *game)
-// {
-// 	t_pos pos;
-	
-// 	if ((code == 2) && game->map[(int)(pos.x + 0.1) / 0.1] == '1')
-// 		return (0);
-// 	else if ((code == 0) && game->map[pos.y / 0.1]
-// 		[(pos.x - 0.1) / 0.1] == '1')
-// 		return (0);
-// 	else if ((code == 13) && game->map[(pos.y - 0.1) / 0.1]
-// 		[pos.x / 0.1] == '1')
-// 		return (0);
-// 	else if ((code == 1) && game->map[(pos.y + 0.1) / 0.1]
-// 		[pos.x / 0.1] == '1')
-// 		return (0);
-// 	return (1);
-// }
-
-int	is_rounded(t_game *game)
+int block_wall(t_game *game, t_pos pos)
 {
-	ft_split(game->map, '\n');
-	// player.x = position du joueur
-	printf("ok\n %s", game->map);
-	return (1);
-}
+	char	**map;
 
-int	is_wall(t_game *game, char **maps, float x, float y)
-{
-	int	rx;
-	int	ry;
-
-	//printf("py is %f\n", data->py);
-	ry = y / game->sy;
-	rx = x / game->sx;
-	//printf("rx is %d, ry is %d\n", rx, ry);
-	//printf("map[%d][%d] is %c\n", ry, rx, maps[ry][rx]);
-	//printf("px is %f, py is %f\n", data->px, data->py);
-	if (maps[ry][rx] && maps[ry][rx] != '1')
+	map = ft_split(game->map, '\n');
+	//debug_split(map);
+	if (map[(int)pos.y][(int)pos.x] && map[(int)pos.y][(int)pos.x + 1] == '1')
 		return (1);
 	return (0);
 }
@@ -108,13 +77,13 @@ void	key_manager(t_game *game, t_key key)
 {
 	//t_pos pos;
 
-	if (key.a == 1 && is_rounded(game))
+	if (key.a == 1 && !block_wall(game, game->player))
 		game->player.x -= 0.1;
-	if (key.s == 1)
+	if (key.s == 1 && !block_wall(game, game->player))
 		game->player.y += 0.1;
-	if (key.w == 1)
+	if (key.w == 1 && !block_wall(game, game->player))
 		game->player.y -= 0.1;
-	if (key.d == 1)
+	if (key.d == 1 && !block_wall(game, game->player))
 		game->player.x += 0.1;
 }
 
