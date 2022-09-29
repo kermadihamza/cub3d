7460c921e3eb5398492c312be171f6d13caadfb6
Louise Rondia <louiserondia@gmail.com>
2022-09-29 20:31:17 +0200

    :construction: Still raycasting but better

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: lrondia <lrondia@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 17:21:00 by lrondia           #+#    #+#             */
-/*   Updated: 2022/09/29 20:28:04 by lrondia          ###   ########.fr       */
+/*   Updated: 2022/09/29 20:30:54 by lrondia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,5 @@
 
 # define WIN_H 1024
 # define WIN_W 1024
-# define V_CHUNCK 1
 
 #endif
